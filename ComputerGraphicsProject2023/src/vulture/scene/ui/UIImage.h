72402a7b9a2d718ee1001f4dd992cb329a97543d
darkhole22 <damiano.guida22@gmail.com>
2023-05-30 10:17:36 +0200

    docs: UIImage documentation

--- ComputerGraphicsProject2023/src/vulture/scene/ui/UIImage.h
+++ ComputerGraphicsProject2023/src/vulture/scene/ui/UIImage.h
@@ -22,14 +22,46 @@ public:
 	UIImage(DescriptorPool& descriptorPool, Ref<DescriptorSetLayout> descriptorSetLayout,
 			const String& image, glm::vec2 position, f32 scale);
 
+	/**
+	 * @brief Change the displayed image to the one indicated.
+	 */
 	void setImage(const String& image);
+
+	/**
+	 * @brief Set the image position to the one provided.
+	 */
 	inline void setPosition(glm::vec2 position) { m_Uniform->position = position; }
+
+	/**
+	 * @brief Set the image position to the one provided.
+	 */
 	inline void setPosition(f32 x, f32 y) { setPosition({ x, y }); }
+
+	/**
+	 * @brief Set the image width to the one provided. This will change the height accordingly
+	 * to keep the correct aspect ratio.
+	 */
 	inline void setWidth(f32 size) { m_Uniform->scale = size / m_Uniform->ratio; }
+
+	/**
+	 * @brief Set the image height to the one provided. This will change the width accordingly
+	 * to keep the correct aspect ratio.
+	 */
 	inline void setHeight(f32 size) { m_Uniform->scale = size; }
+
+	/**
+	 * @brief Shows or hides the image.
+	 */
 	void setVisible(bool visible);
 
+	/**
+	 * @brief Returns the position of the image.
+	 */
 	inline glm::vec2 getPosition() const { return m_Uniform->position; }
+
+	/**
+	 * @brief Returns the aspect ratio of the image.
+	 */
 	inline f32 getAspectRatio() const { return m_Uniform->ratio; }
 
 	/**
@@ -42,6 +74,9 @@ public:
 	 */
 	inline f32 getHeight() const { return m_Uniform->scale; }
 
+	/**
+	 * @brief Returns true if the image is visible.
+	 */
 	inline bool isVisible() const { return m_Visible; }
 
 	friend class UIHandler;
