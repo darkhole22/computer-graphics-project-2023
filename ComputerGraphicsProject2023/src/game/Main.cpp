5cc67d860b2adc48b965a79aa0d6846c767a92c5
darkhole22 <damiano.guida22@gmail.com>
2023-07-19 12:41:20 +0200

    fix: formatting

--- ComputerGraphicsProject2023/src/game/Main.cpp
+++ ComputerGraphicsProject2023/src/game/Main.cpp
@@ -46,16 +46,16 @@ public:
 		/***********
 		 * LOADING *
 		 ***********/
-		Job::submit([](void *) -> bool {
+		Job::submit([](void*) -> bool {
 			using namespace std::chrono_literals;
 
 			std::this_thread::sleep_for(100ms);
 			return true;
-		},nullptr,[this](bool, void *) {
+		}, nullptr, [this](bool, void*) {
 			/***********
-		   	* TERRAIN *
-		   	***********/
-		  	TerrainGenerationConfig terrainConfig{};
+			* TERRAIN *
+			***********/
+			TerrainGenerationConfig terrainConfig{};
 			terrainConfig.noiseScale = 2;
 			terrainConfig.heightScale = 250;
 
@@ -94,8 +94,8 @@ private:
 	static inline const String SKYBOX_MIDNIGHT = "skybox/SkyMidNight";
 	static inline const String SKYBOX_NIGHT = "skybox/SkyNight";
 
-	Window *m_Window = nullptr;
-	Scene *m_Scene = nullptr;
+	Window* m_Window = nullptr;
+	Scene* m_Scene = nullptr;
 
 	Ref<DebugUI> m_DebugUI = nullptr;
 	Ref<HUD> m_HUD = nullptr;
@@ -121,27 +121,27 @@ private:
 			m_Scene->getWorld()->directLight.direction = glm::vec3(cos(angle) * cos(hAngle), sin(angle) * cos(hAngle), sin(hAngle));
 		};
 
-		auto *directLightTween = m_Scene->makeTween()->loop();
+		auto* directLightTween = m_Scene->makeTween()->loop();
 
 		// Morning
 		directLightTween->addCallbackTweener([this]() { m_Scene->setSkybox(SKYBOX_MORNING); });
-		directLightTween->addMethodTweener<f32>(lightFun, 0.0f, glm::radians(30.0f),15.0f);
+		directLightTween->addMethodTweener<f32>(lightFun, 0.0f, glm::radians(30.0f), 15.0f);
 
 		// Day
 		directLightTween->addCallbackTweener([this]() { m_Scene->setSkybox(SKYBOX_DAY); });
-		directLightTween->addMethodTweener<f32>(lightFun, glm::radians(30.0f),glm::radians(150.0f), 60.0f);
+		directLightTween->addMethodTweener<f32>(lightFun, glm::radians(30.0f), glm::radians(150.0f), 60.0f);
 
 		// Sunset
 		directLightTween->addCallbackTweener([this]() { m_Scene->setSkybox(SKYBOX_SUNSET); });
-		directLightTween->addMethodTweener<f32>(lightFun, glm::radians(150.0f),glm::radians(180.0f), 10.0f);
+		directLightTween->addMethodTweener<f32>(lightFun, glm::radians(150.0f), glm::radians(180.0f), 10.0f);
 
 		// Midnight
 		directLightTween->addCallbackTweener([this]() { m_Scene->setSkybox(SKYBOX_MIDNIGHT); });
-		directLightTween->addMethodTweener<f32>(lightInvFun, glm::radians(180.0f),glm::radians(30.0f), 10.0f);
+		directLightTween->addMethodTweener<f32>(lightInvFun, glm::radians(180.0f), glm::radians(30.0f), 10.0f);
 
 		// Night
 		directLightTween->addCallbackTweener([this]() { m_Scene->setSkybox(SKYBOX_NIGHT); });
-		directLightTween->addMethodTweener<f32>(lightInvFun, glm::radians(150.0f),0.0f, 50.0f);
+		directLightTween->addMethodTweener<f32>(lightInvFun, glm::radians(150.0f), 0.0f, 50.0f);
 	}
 
 	void setupInputActions()
@@ -150,58 +150,58 @@ private:
 		 *                   UI                       *
 		 **********************************************/
 		InputAction pauseAction{};
-		pauseAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_ESCAPE}}};
+		pauseAction.keyboardBindings = { KeyboardBinding{{GLFW_KEY_ESCAPE}} };
 		pauseAction.gamepadButtonBindings = {
-			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_START}}};
+			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_START}} };
 		Input::setAction("TOGGLE_PAUSE", pauseAction);
 
 		InputAction restartAction{};
-		restartAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_R}}};
+		restartAction.keyboardBindings = { KeyboardBinding{{GLFW_KEY_R}} };
 		restartAction.gamepadButtonBindings = {
-			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_START}}};
+			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_START}} };
 		Input::setAction("RESTART", restartAction);
 
 		/**********************************************
 		 *                MOVEMENT                    *
 		 **********************************************/
 		InputAction leftAction{};
-		leftAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_A}}};
+		leftAction.keyboardBindings = { KeyboardBinding{{GLFW_KEY_A}} };
 		leftAction.gamepadAxisBindings = {
-			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_X, AXIS_NEG}}}};
+			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_X, AXIS_NEG}}} };
 		Input::setAction("MOVE_LEFT", leftAction);
 
 		InputAction rightAction{};
-		rightAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_D}}};
+		rightAction.keyboardBindings = { KeyboardBinding{{GLFW_KEY_D}} };
 		rightAction.gamepadAxisBindings = {
-			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_X, AXIS_POS}}}};
+			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_X, AXIS_POS}}} };
 		Input::setAction("MOVE_RIGHT", rightAction);
 
 		InputAction upAction{};
-		upAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_W}}};
+		upAction.keyboardBindings = { KeyboardBinding{{GLFW_KEY_W}} };
 		upAction.gamepadAxisBindings = {
-			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_Y, AXIS_NEG}}}};
+			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_Y, AXIS_NEG}}} };
 		Input::setAction("MOVE_UP", upAction);
 
 		InputAction downAction{};
-		downAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_S}}};
+		downAction.keyboardBindings = { KeyboardBinding{{GLFW_KEY_S}} };
 		downAction.gamepadAxisBindings = {
-			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_Y, AXIS_POS}}}};
+			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_Y, AXIS_POS}}} };
 		Input::setAction("MOVE_DOWN", downAction);
 
 		/**********************************************
 		 *                  ACTIONS                   *
 		 **********************************************/
 		InputAction fireAction{};
-		fireAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_SPACE}}};
-		fireAction.mouseBindings = {MouseBinding{{GLFW_MOUSE_BUTTON_1}}};
+		fireAction.keyboardBindings = { KeyboardBinding{{GLFW_KEY_SPACE}} };
+		fireAction.mouseBindings = { MouseBinding{{GLFW_MOUSE_BUTTON_1}} };
 		fireAction.gamepadButtonBindings = {
-			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER}}};
+			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER}} };
 		Input::setAction("FIRE", fireAction);
 
 		InputAction dashAction{};
-		dashAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_LEFT_SHIFT}}};
+		dashAction.keyboardBindings = { KeyboardBinding{{GLFW_KEY_LEFT_SHIFT}} };
 		dashAction.gamepadButtonBindings = {
-			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_LEFT_BUMPER}}};
+			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_LEFT_BUMPER}} };
 		Input::setAction("DASH", dashAction);
 
 		/**********************************************
@@ -213,8 +213,8 @@ private:
 			KeyboardBinding{{GLFW_KEY_LEFT}},
 		};
 		lookLeftAction.gamepadAxisBindings = {
-			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_X, AXIS_NEG}}}};
-		lookLeftAction.mouseAxisBindings = {MouseAxisBinding{{Axis::NEGATIVE_X}}};
+			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_X, AXIS_NEG}}} };
+		lookLeftAction.mouseAxisBindings = { MouseAxisBinding{{Axis::NEGATIVE_X}} };
 		Input::setAction("LOOK_LEFT", lookLeftAction);
 
 		InputAction lookRightAction{};
@@ -222,8 +222,8 @@ private:
 			KeyboardBinding{{GLFW_KEY_RIGHT}},
 		};
 		lookRightAction.gamepadAxisBindings = {
-			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_X, AXIS_POS}}}};
-		lookRightAction.mouseAxisBindings = {MouseAxisBinding{{Axis::POSITIVE_X}}};
+			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_X, AXIS_POS}}} };
+		lookRightAction.mouseAxisBindings = { MouseAxisBinding{{Axis::POSITIVE_X}} };
 		Input::setAction("LOOK_RIGHT", lookRightAction);
 
 		InputAction lookUpAction{};
@@ -231,8 +231,8 @@ private:
 			KeyboardBinding{{GLFW_KEY_UP}},
 		};
 		lookUpAction.gamepadAxisBindings = {
-			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_Y, AXIS_NEG}}}};
-		lookUpAction.mouseAxisBindings = {MouseAxisBinding{{Axis::NEGATIVE_Y}}};
+			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_Y, AXIS_NEG}}} };
+		lookUpAction.mouseAxisBindings = { MouseAxisBinding{{Axis::NEGATIVE_Y}} };
 		Input::setAction("LOOK_UP", lookUpAction);
 
 		InputAction lookDownAction{};
@@ -240,8 +240,8 @@ private:
 			KeyboardBinding{{GLFW_KEY_DOWN}},
 		};
 		lookDownAction.gamepadAxisBindings = {
-			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_Y, AXIS_POS}}}};
-		lookDownAction.mouseAxisBindings = {MouseAxisBinding{{Axis::POSITIVE_Y}}};
+			GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_Y, AXIS_POS}}} };
+		lookDownAction.mouseAxisBindings = { MouseAxisBinding{{Axis::POSITIVE_Y}} };
 		Input::setAction("LOOK_DOWN", lookDownAction);
 
 		/**********************************************
@@ -253,7 +253,7 @@ private:
 			KeyboardBinding{{GLFW_KEY_F3}},
 		};
 		toggleInfoAction.gamepadButtonBindings = {
-			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_BACK}}};
+			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_BACK}} };
 		Input::setAction("TOGGLE_INFO", toggleInfoAction);
 
 		InputAction toggleGodmodeAction{};
@@ -267,7 +267,7 @@ private:
 			KeyboardBinding{{GLFW_KEY_M}},
 		};
 		toggleSkyboxAction.gamepadButtonBindings = {
-			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_Y}}};
+			GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_Y}} };
 		Input::setAction("TOGGLE_SKYBOX", toggleSkyboxAction);
 	}
 };
@@ -285,12 +285,12 @@ int main()
 		{
 			game::TestGame game;
 
-			app = Application::launch(game,vulture::AppConfig{"Vulture demo", 800, 600});
+			app = Application::launch(game, vulture::AppConfig{ "Vulture demo", 800, 600 });
 		}
 
 		return EXIT_SUCCESS;
 	}
-	catch (const std::exception &exception)
+	catch (const std::exception& exception)
 	{
 		VUFATAL("An exception was thrown.\nMessage: %s\n\nShutting down!", exception.what());
 	}
