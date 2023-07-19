297d001e68cda771c3f63a18e42adb47196d33f2
Michele Giornetta <michelegiornetta@gmail.com>
2023-07-19 16:38:42 +0200

    docs: DescriptorSet and SwapChain

--- ComputerGraphicsProject2023/src/vulture/renderer/DescriptorSet.h
+++ ComputerGraphicsProject2023/src/vulture/renderer/DescriptorSet.h
@@ -9,6 +9,11 @@
 
 namespace vulture {
 
+/**
+* @class DescriptorSetLayout
+*
+* @brief Represents a Vulkan descriptor set layout used for describing descriptor sets.
+*/
 class DescriptorSetLayout
 {
 public:
@@ -16,10 +21,34 @@ public:
 
 	DescriptorSetLayout() = default;
 
+	/**
+	 * @brief Adds a binding to the descriptor set layout.
+	 *
+	 * @param type The type of descriptor (e.g., uniform buffer, combined image sampler).
+	 * @param target The shader stage that the descriptor is intended for (e.g., vertex shader, fragment shader).
+	 * @param count The number of descriptors in the binding (default is 1).
+	 */
 	void addBinding(VkDescriptorType type, VkShaderStageFlags target, u32 count = 1);
+
+	/**
+	 * @brief Creates the descriptor set layout.
+	 *
+	 * @return True if the descriptor set layout is successfully created; otherwise, false.
+	 */
 	bool create();
 
+	/**
+	 * @brief Gets the Vulkan handle of the descriptor set layout.
+	 *
+	 * @return The Vulkan handle of the descriptor set layout.
+	 */
 	inline VkDescriptorSetLayout getHandle() const { return m_Handle; }
+
+	/**
+	 * @brief Gets the bindings of the descriptor set layout.
+	 *
+	 * @return A constant reference to the vector of descriptor set layout bindings.
+	 */
 	inline const auto& getBindings() const { return m_Bindings; }
 
 	~DescriptorSetLayout();
@@ -28,6 +57,12 @@ private:
 	std::vector<VkDescriptorSetLayoutBinding> m_Bindings;
 };
 
+/**
+ * @class Uniform
+ *
+ * @brief Represents a uniform buffer used for descriptor sets in a rendering pipeline.
+ *        Templated class to allow different types of uniform data.
+ */
 template <class Type>
 class Uniform
 {
@@ -115,8 +150,21 @@ public:
 		m_TextureInfo = imageInfo;
 	}
 
+	/**
+	 * @brief Gets the Vulkan write descriptor set structure for the descriptor set update.
+	 *
+	 * @param descriptorSet The Vulkan handle of the descriptor set to be updated.
+	 * @param index The index of the uniform buffer (frame) associated with the descriptor set.
+	 * @param binding The binding index of the descriptor set layout to be updated.
+	 * @return The Vulkan write descriptor set structure for the descriptor set update.
+	 */
 	VkWriteDescriptorSet getWriteDescriptorSet(VkDescriptorSet descriptorSet, u32 index, u32 binding) const;
 
+	/**
+	 * @brief Maps the uniform buffer or texture sampler associated with the descriptor set.
+	 *
+	 * @param index The index of the uniform buffer (frame) associated with the descriptor set.
+	 */
 	void map(u32 index) const;
 
 	~DescriptorWrite() = default;
@@ -126,31 +174,68 @@ private:
 	std::optional<VkDescriptorImageInfo> m_TextureInfo;
 };
 
-/*
-*/
+/**
+ * @class DescriptorPool
+ *
+ * @brief Represents a Vulkan descriptor pool used for allocating descriptor sets.
+ */
 class DescriptorPool
 {
 public:
 	NO_COPY(DescriptorPool)
 
-	/*
+	/**
 	* @brief Constructor of a DescriptorPool
 	*
-	* @param frameCount - The number of framebuffers.
+	* @param frameCount The number of framebuffers.
 	*/
 	explicit DescriptorPool(u32 frameCount);
 
+	/**
+	 * @brief Gets the number of framebuffers associated with the descriptor pool.
+	 *
+	 * @return The number of framebuffers.
+	 */
 	inline u32 getFrameCount() const { return m_FrameCount; }
+
+	/**
+	 * @brief Sets the number of framebuffers associated with the descriptor pool.
+	 *
+	 * @param frameCount The number of framebuffers to set.
+	 */
 	void setFrameCount(u32 frameCount);
 
+	/**
+	 * @brief Gets the Vulkan handle of the descriptor pool.
+	 *
+	 * @return The Vulkan handle of the descriptor pool.
+	 */
 	inline VkDescriptorPool getHandle() const { return m_Handle; }
 
+	/**
+	 * @brief Reserves space in the descriptor pool for the specified number of descriptors with the given layout.
+	 *
+	 * @param count The number of descriptors to reserve space for.
+	 * @param layout The descriptor set layout to use for the descriptors.
+	 */
 	void reserveSpace(u32 count, Ref<DescriptorSetLayout> layout);
 
+	/**
+	 * @brief Gets a descriptor set from the descriptor pool with the specified layout and descriptor writes.
+	 *
+	 * @param layout The descriptor set layout for the descriptor set.
+	 * @param descriptorWrites The descriptor writes used to update the descriptor set with data.
+	 * @return A reference to the created DescriptorSet instance.
+	 */
 	Ref<DescriptorSet> getDescriptorSet(Ref<DescriptorSetLayout> layout, const std::vector<DescriptorWrite>& descriptorWrites);
 
 	~DescriptorPool();
 
+	/**
+	 * @struct DescriptorTypePoolInfo
+	 *
+	 * @brief Contains information about descriptor types in the descriptor pool.
+	 */
 	struct DescriptorTypePoolInfo
 	{
 		u32 size;
@@ -170,15 +255,36 @@ private:
 	bool recreate();
 };
 
+/**
+ * @class DescriptorSet
+ *
+ * @brief Represents a Vulkan descriptor set allocated from a descriptor pool.
+ */
 class DescriptorSet
 {
 public:
 	NO_COPY(DescriptorSet)
 
+	/**
+	 * @brief Gets the descriptor set layout associated with the descriptor set.
+	 *
+	 * @return A constant reference to the DescriptorSetLayout instance.
+	 */
 	inline const DescriptorSetLayout& getLayout() const { return *m_Layout; }
 
+	/**
+	 * @brief Gets the Vulkan handle of the descriptor set at the specified index.
+	 *
+	 * @param index The index of the descriptor set in the handles array.
+	 * @return The Vulkan handle of the descriptor set at the specified index.
+	 */
 	inline VkDescriptorSet getHandle(u32 index) const { return m_Handles[index]; }
 
+	/**
+	 * @brief Maps the descriptor set for the specified index to update its contents.
+	 *
+	 * @param index The index of the descriptor set to be mapped.
+	 */
 	void map(u32 index) const;
 
 	~DescriptorSet();
--- ComputerGraphicsProject2023/src/vulture/renderer/SwapChain.h
+++ ComputerGraphicsProject2023/src/vulture/renderer/SwapChain.h
@@ -7,23 +7,82 @@
 
 namespace vulture {
 
-const int MAX_FRAMES_IN_FLIGHT = 2;
+const int MAX_FRAMES_IN_FLIGHT = 2; /**< Maximum number of frames in flight (double buffering). */
 
+/**
+ * @class SwapChain
+ *
+ * @brief Represents a Vulkan swap chain used for rendering images to the screen.
+ */
 class SwapChain
 {
 public:
-	SwapChain(const Window& window);
+	/**
+	 * @brief Constructor for the SwapChain class.
+	 *
+	 * @param window The window associated with the swap chain.
+	 */
+	explicit SwapChain(const Window& window);
 
+	/**
+	 * @brief Attaches the specified RenderPass to the swap chain.
+	 *
+	 * @param renderPass The RenderPass to be attached.
+	 * @return True if the RenderPass is successfully attached; otherwise, false.
+	 */
 	bool attachRenderPass(const RenderPass& renderPass);
 
+	/**
+	 * @brief Gets the image format used by the swap chain.
+	 *
+	 * @return The Vulkan format of the swap chain images.
+	 */
 	inline VkFormat getImageFormat() const { return m_ImageFormat; }
 
+	/**
+	 * @brief Gets the index of the image to be used for the current frame.
+	 *
+	 * @param currentFrame The index of the current frame.
+	 * @return The index of the image to be used for rendering in the current frame.
+	 */
 	u32 getImageIndex(u32 currentFrame);
+
+
+	/**
+	 * @brief Gets the CommandBuffer to be used for rendering in the current frame.
+	 *
+	 * @param currentFrame The index of the current frame.
+	 * @return The CommandBuffer to be used for rendering in the current frame.
+	 */
 	CommandBuffer& getCommandBuffer(u32 currentFrame);
+
+	/**
+	 * @brief Submits the current frame for rendering.
+	 *
+	 * @param currentFrame The index of the current frame.
+	 * @param imageIndex The index of the image used for rendering in the current frame.
+	 */
 	void submit(u32 currentFrame, u32 imageIndex);
 
+	/**
+	 * @brief Gets the extent (width and height) of the swap chain images.
+	 *
+	 * @return The extent of the swap chain images.
+	 */
 	inline const VkExtent2D& getExtent() const { return m_Extent; }
+
+	/**
+	 * @brief Gets the total number of images in the swap chain.
+	 *
+	 * @return The total number of images in the swap chain.
+	 */
 	inline u32 getImageCount() const { return static_cast<u32>(m_Images.size()); }
+
+	/**
+	 * @brief Checks if the swap chain was recreated.
+	 *
+	 * @return True if the swap chain was recreated; otherwise, false.
+	 */
 	inline bool wasRecreated() const { return m_Recreated; }
 
 	~SwapChain();
@@ -36,10 +95,10 @@ private:
 	std::vector<Image> m_Images;
 	Image m_ColorImage;
 	Image m_DepthImage;
-	std::array<VkSemaphore, MAX_FRAMES_IN_FLIGHT> m_ImageAvailableSemaphores;
-	std::array<VkSemaphore, MAX_FRAMES_IN_FLIGHT> m_RenderFinishedSemaphores;
-	std::array<VkFence, MAX_FRAMES_IN_FLIGHT> m_InFlightFences;
-	std::vector<VkFence> m_ImageInFlightFences;
+	std::array<VkSemaphore, MAX_FRAMES_IN_FLIGHT> m_ImageAvailableSemaphores; /**< Semaphores for image availability. */
+	std::array<VkSemaphore, MAX_FRAMES_IN_FLIGHT> m_RenderFinishedSemaphores; /**< Semaphores for rendering completion. */
+	std::array<VkFence, MAX_FRAMES_IN_FLIGHT> m_InFlightFences; /**< Fences for synchronization between CPU and GPU. */
+	std::vector<VkFence> m_ImageInFlightFences; /**< Fences for synchronization with each image in the swap chain. */
 	std::vector<CommandBuffer> m_CommandBuffers;
 
 	RenderPass const* m_RenderPass = nullptr;
