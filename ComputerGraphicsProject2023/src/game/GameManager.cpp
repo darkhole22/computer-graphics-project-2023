7e1dd126b1e740851d22f3a5f5ee58870a498ce9
Michele Giornetta <michelegiornetta@gmail.com>
2023-07-19 12:19:13 +0200

    feat: implement multiple skyboxes

--- ComputerGraphicsProject2023/src/game/GameManager.cpp
+++ ComputerGraphicsProject2023/src/game/GameManager.cpp
@@ -1,145 +1,138 @@
 #include "GameManager.h"
 
-#include "vulture/util/ScopeTimer.h"
-#include "vulture/util/Random.h"
 #include "vulture/core/Input.h"
+#include "vulture/util/Random.h"
+#include "vulture/util/ScopeTimer.h"
 
 namespace game {
 
-GameManager::GameManager(const TerrainGenerationConfig& terrainConfig) :
-	m_Scene(Application::getScene()), m_Terrain(makeRef<Terrain>(terrainConfig)),
-	m_Player(makeRef<Player>(m_Terrain)),
-	m_EnemyFactory(m_EnemyWaveSize, glm::rotate(glm::mat4(1), glm::half_pi<f32>(), glm::vec3(0, 1, 0))),
-	m_PowerUpManager(m_Player, m_Terrain),
-	m_DeathAudio("lose")
-{
-	EventBus::addCallback([this](HealthUpdated event) {
-		if (event.hp != 0) return;
-		onGameOver();
-	});
+GameManager::GameManager(const TerrainGenerationConfig &terrainConfig)
+    : m_Scene(Application::getScene()),
+      m_Terrain(makeRef<Terrain>(terrainConfig)),
+      m_Player(makeRef<Player>(m_Terrain)),
+      m_EnemyFactory(
+          m_EnemyWaveSize,
+          glm::rotate(glm::mat4(1), glm::half_pi<f32>(), glm::vec3(0, 1, 0))),
+      m_PowerUpManager(m_Player, m_Terrain), m_DeathAudio("lose") {
+  EventBus::addCallback([this](HealthUpdated event) {
+    if (event.hp != 0)
+      return;
+    onGameOver();
+  });
 
-	EventBus::addCallback([this](EnemyDied e) { onEnemyDied(e); });
-	EventBus::addCallback([this](DoubleScoreStarted e) { onDoubleScoreStarted(e); });
+  EventBus::addCallback([this](EnemyDied e) { onEnemyDied(e); });
+  EventBus::addCallback(
+      [this](DoubleScoreStarted e) { onDoubleScoreStarted(e); });
 
-	m_WaveTimer = m_Scene->makeTimer(20, false);
-	m_WaveTimer->addCallback([this](const TimerTimeoutEvent&) {
-		for (u32 i = 0; i < m_EnemyWaveSize; i++)
-		{
-			auto spawnPointOffset = Random::nextAnnulusPoint(100.0f);
-			auto enemy = m_EnemyFactory.get();
-			if (!enemy) break;
+  m_WaveTimer = m_Scene->makeTimer(20, false);
+  m_WaveTimer->addCallback([this](const TimerTimeoutEvent &) {
+    for (u32 i = 0; i < m_EnemyWaveSize; i++) {
+      auto spawnPointOffset = Random::nextAnnulusPoint(100.0f);
+      auto enemy = m_EnemyFactory.get();
+      if (!enemy)
+        break;
 
-			auto spawnPoint = m_Player->getPosition() + glm::vec3(spawnPointOffset.x, 0.0f, spawnPointOffset.y);
-			enemy->setup(m_Player, m_Terrain, spawnPoint);
-		}
-	});
-	m_WaveTimer->pause();
+      auto spawnPoint = m_Player->getPosition() +
+                        glm::vec3(spawnPointOffset.x, 0.0f, spawnPointOffset.y);
+      enemy->setup(m_Player, m_Terrain, spawnPoint);
+    }
+  });
+  m_WaveTimer->pause();
 
-	setGameState(GameState::TITLE);
+  setGameState(GameState::TITLE);
 }
 
-void GameManager::update(f32 dt)
-{
-	switch (m_GameState)
-	{
-	case GameState::TITLE:
-	if (Input::isActionJustPressed("FIRE"))
-	{
-		setGameState(GameState::SETUP);
-	}
-	break;
-	case GameState::SETUP:
-	{
-		m_Score = 0;
-		EventBus::emit(ScoreUpdated{ m_Score });
+void GameManager::update(f32 dt) {
+  switch (m_GameState) {
+  case GameState::TITLE:
+    if (Input::isActionJustPressed("FIRE")) {
+      setGameState(GameState::SETUP);
+    }
+    break;
+  case GameState::SETUP: {
+    m_Score = 0;
+    EventBus::emit(ScoreUpdated{m_Score});
 
-		m_WaveTimer->play();
-		m_PowerUpManager.start();
+    m_WaveTimer->play();
+    m_PowerUpManager.start();
 
-		setGameState(GameState::PLAYING);
-		break;
-	}
-	case GameState::PLAYING:
-	{
-		m_EnemyFactory.update(dt);
-		m_PowerUpManager.update(dt);
-		m_Player->update(dt);
+    setGameState(GameState::PLAYING);
+    break;
+  }
+  case GameState::PLAYING: {
+    m_EnemyFactory.update(dt);
+    m_PowerUpManager.update(dt);
+    m_Player->update(dt);
 
-		if (Input::isActionJustPressed("TOGGLE_PAUSE"))
-		{
-			m_WaveTimer->pause();
-			m_PowerUpManager.pause();
+    if (Input::isActionJustPressed("TOGGLE_PAUSE")) {
+      m_WaveTimer->pause();
+      m_PowerUpManager.pause();
 
-			setGameState(GameState::PAUSE);
-		}
+      setGameState(GameState::PAUSE);
+    }
 
-		break;
-	}
-	case GameState::PAUSE:
-	if (Input::isActionJustPressed("TOGGLE_PAUSE"))
-	{
-		m_WaveTimer->play();
-		m_PowerUpManager.start();
+    break;
+  }
+  case GameState::PAUSE:
+    if (Input::isActionJustPressed("TOGGLE_PAUSE")) {
+      m_WaveTimer->play();
+      m_PowerUpManager.start();
 
-		setGameState(GameState::PLAYING);
-	}
-	break;
-	case GameState::GAME_OVER:
-	if (Input::isActionJustPressed("RESTART"))
-	{
-		m_DeathAudio.stop();
-		m_EnemyFactory.reset();
-		m_PowerUpManager.reset();
-		m_Player->reset();
+      setGameState(GameState::PLAYING);
+    }
+    break;
+  case GameState::GAME_OVER:
+    if (Input::isActionJustPressed("RESTART")) {
+      m_DeathAudio.stop();
+      m_EnemyFactory.reset();
+      m_PowerUpManager.reset();
+      m_Player->reset();
 
-		setGameState(GameState::SETUP);
-	}
-	break;
-	}
+      setGameState(GameState::SETUP);
+    }
+    break;
+  }
 
-	auto cameraPos = m_Scene->getCamera()->position;
-	m_Terrain->setReferencePosition({ cameraPos.x, cameraPos.z });
+  auto cameraPos = m_Scene->getCamera()->position;
+  m_Terrain->setReferencePosition({cameraPos.x, cameraPos.z});
 
-	m_Terrain->update(dt);
+  m_Terrain->update(dt);
 }
 
-void GameManager::setGameState(GameState gameState)
-{
-	if (gameState == m_GameState) return;
+void GameManager::setGameState(GameState gameState) {
+  if (gameState == m_GameState)
+    return;
 
-	m_GameState = gameState;
-	EventBus::emit(GameStateChanged{ m_GameState });
+  m_GameState = gameState;
+  EventBus::emit(GameStateChanged{m_GameState});
 
-	Application::getWindow()->setCursorMode(m_GameState == GameState::PLAYING ?
-											CursorMode::DISABLED :
-											CursorMode::NORMAL);
+  Application::getWindow()->setCursorMode(m_GameState == GameState::PLAYING
+                                              ? CursorMode::DISABLED
+                                              : CursorMode::NORMAL);
 }
 
-void GameManager::onGameOver()
-{
-	m_WaveTimer->reset();
-	m_PowerUpManager.pause();
+void GameManager::onGameOver() {
+  m_WaveTimer->reset();
+  m_PowerUpManager.pause();
 
-	if (m_GameState != GameState::GAME_OVER)
-		m_DeathAudio.play();
+  if (m_GameState != GameState::GAME_OVER)
+    m_DeathAudio.play();
 
-	setGameState(GameState::GAME_OVER);
+  setGameState(GameState::GAME_OVER);
 }
 
-void GameManager::onEnemyDied(EnemyDied event)
-{
-	m_Score += m_Player->getDoubleScoreActive() ? 200 : 100;
-	EventBus::emit(ScoreUpdated{ m_Score });
+void GameManager::onEnemyDied(EnemyDied event) {
+  m_Score += m_Player->getDoubleScoreActive() ? 200 : 100;
+  EventBus::emit(ScoreUpdated{m_Score});
 }
 
-void GameManager::onDoubleScoreStarted(DoubleScoreStarted e)
-{
-	m_Scene->makeTimer(e.duration)->addCallback([this](const TimerTimeoutEvent&) {
-		if (m_Player->getDoubleScoreActive())
-		{
-			EventBus::emit(DoubleScoreOver{});
-		}
-	});
+void GameManager::onDoubleScoreStarted(DoubleScoreStarted e) {
+  m_Scene->makeTimer(e.duration)
+      ->addCallback([this](const TimerTimeoutEvent &) {
+        if (m_Player->getDoubleScoreActive()) {
+          EventBus::emit(DoubleScoreOver{});
+        }
+      });
 }
 
 } // namespace game
--- ComputerGraphicsProject2023/src/game/Main.cpp
+++ ComputerGraphicsProject2023/src/game/Main.cpp
@@ -1,291 +1,300 @@
 #define VU_LOGGER_TRACE_ENABLED
-#include "vulture/core/Logger.h"
 #include "vulture/core/Application.h"
-#include "vulture/util/ScopeTimer.h"
 #include "vulture/core/Job.h"
+#include "vulture/core/Logger.h"
+#include "vulture/util/ScopeTimer.h"
 
 #include "game/GameManager.h"
-#include "game/ui/DebugUI.h"
 #include "game/terrain/Terrain.h"
+#include "game/ui/DebugUI.h"
 
 namespace game {
 
 using namespace vulture;
 
-class TestGame : public Game
-{
+class TestGame : public Game {
 public:
+  void setup() override {
+    /**********
+     *  SETUP *
+     **********/
+    m_Window = Application::getWindow();
+    m_Window->setCursorMode(CursorMode::NORMAL);
 
-	void setup() override
-	{
-		/**********
-		 *  SETUP *
-		 **********/
-		m_Window = Application::getWindow();
-		m_Window->setCursorMode(CursorMode::NORMAL);
+    m_Scene = Application::getScene();
+    EventBus::init();
+    setupInputActions();
 
-		m_Scene = Application::getScene();
-		EventBus::init();
-		setupInputActions();
+    /**********
+     * SKYBOX *
+     **********/
+    m_Scene->setSkybox(SKYBOX_DAY);
 
-		/**********
-		 * SKYBOX *
-		 **********/
-		m_Scene->setSkybox(SKYBOX_NAME);
+    /**********
+     *   UI   *
+     **********/
+    m_DebugUI = makeRef<DebugUI>();
+    m_HUD = makeRef<HUD>();
 
-		/**********
-		 *   UI   *
-		 **********/
-		m_DebugUI = makeRef<DebugUI>();
-		m_HUD = makeRef<HUD>();
+    /*********
+     * LIGHT *
+     *********/
+    setupLight();
 
-		/*********
-		 * LIGHT *
-		 *********/
-		setupLight();
+    /***********
+     * LOADING *
+     ***********/
+    Job::submit(
+        [](void *) -> bool {
+          using namespace std::chrono_literals;
 
-		/***********
-		 * LOADING *
-		 ***********/
-		Job::submit([](void*)->bool {
-			using namespace std::chrono_literals;
+          std::this_thread::sleep_for(100ms);
+          return true;
+        },
+        nullptr,
+        [this](bool, void *) {
+          /***********
+           * TERRAIN *
+           ***********/
+          TerrainGenerationConfig terrainConfig{};
+          terrainConfig.noiseScale = 2;
+          terrainConfig.heightScale = 250;
 
-			std::this_thread::sleep_for(100ms);
-			return true;
-		}, nullptr, [this](bool, void*) {
-			/***********
-			 * TERRAIN *
-			 ***********/
-			TerrainGenerationConfig terrainConfig{};
-			terrainConfig.noiseScale = 2;
-			terrainConfig.heightScale = 250;
+          /**************
+           * GAME LOGIC *
+           **************/
+          m_GameManager = makeRef<GameManager>(terrainConfig);
 
-			/**************
-			 * GAME LOGIC *
-			 **************/
-			m_GameManager = makeRef<GameManager>(terrainConfig);
+          m_HUD->loadingEnded();
+        });
+  }
 
-			m_HUD->loadingEnded();
-		});
-	}
+  void update(float dt) override {
+    if (Input::isActionJustPressed("TOGGLE_SKYBOX")) {
+      static bool useSkybox = false;
+      m_Scene->setSkybox(useSkybox ? SKYBOX_DAY : "");
+      useSkybox = !useSkybox;
+    }
 
-	void update(float dt) override
-	{
-		if (Input::isActionJustPressed("TOGGLE_SKYBOX"))
-		{
-			static bool useSkybox = false;
-			m_Scene->setSkybox(useSkybox ? SKYBOX_NAME : "");
-			useSkybox = !useSkybox;
-		}
+    if (m_GameManager)
+      m_GameManager->update(dt);
+    m_DebugUI->update(dt);
+  }
 
-		if (m_GameManager) m_GameManager->update(dt);
-		m_DebugUI->update(dt);
-	}
+  ~TestGame() {
+    m_GameManager.reset();
+    EventBus::cleanup();
+  }
 
-	~TestGame()
-	{
-		m_GameManager.reset();
-		EventBus::cleanup();
-	}
 private:
-	static inline const String SKYBOX_NAME = "desert";
+  static inline const String SKYBOX_MORNING = "skybox/SkyMorning";
+  static inline const String SKYBOX_DAY = "skybox/SkyAfterNoon";
+  static inline const String SKYBOX_SUNSET = "skybox/SkySunSet";
+  static inline const String SKYBOX_MIDNIGHT = "skybox/SkyMidNight";
+  static inline const String SKYBOX_NIGHT = "skybox/SkyNight";
 
-	Window* m_Window = nullptr;
-	Scene* m_Scene = nullptr;
+  Window *m_Window = nullptr;
+  Scene *m_Scene = nullptr;
 
-	Ref<DebugUI> m_DebugUI = nullptr;
-	Ref<HUD> m_HUD = nullptr;
-	Ref<GameManager> m_GameManager = nullptr;
+  Ref<DebugUI> m_DebugUI = nullptr;
+  Ref<HUD> m_HUD = nullptr;
+  Ref<GameManager> m_GameManager = nullptr;
 
-	void setupLight()
-	{
-		m_Scene->getWorld()->directLight.color = glm::vec4(1.0f);
-		m_Scene->getWorld()->directLight.direction = glm::normalize(glm::vec3(-1.0f));
+  void setupLight() {
+    m_Scene->getWorld()->directLight.color = glm::vec4(1.0f);
+    m_Scene->getWorld()->directLight.direction =
+        glm::normalize(glm::vec3(-1.0f));
 
-		auto* directLightTween = m_Scene->makeTween()->loop();
-		directLightTween->addMethodTweener<f32>([this](f32 angle) {
-			constexpr float hAngle = glm::radians(10.0f);
-			m_Scene->getWorld()->directLight.color =
-				glm::vec4(1.0f, 0.2f + 0.7f * std::pow(sin(angle), 3.0f), std::pow(sin(angle), 5.0f) * 0.9f, 1.0f);
-			m_Scene->getWorld()->directLight.direction = glm::vec3(cos(angle) * cos(hAngle), sin(angle) * cos(hAngle), sin(hAngle));
-		}, 0.0f, glm::radians(180.0f), 60.0f);
-		directLightTween->addMethodTweener<f32>([this](f32 angle) {
-			constexpr float hAngle = glm::radians(10.0f);
-			f32 decay = 1.0f - std::pow(sin(angle), 5.0f);
-			m_Scene->getWorld()->directLight.color = static_cast<f32>(1.0f - 0.5f * sin(angle)) *
-				glm::vec4(decay * 1.0f, decay * 0.2f, 1.0f - decay, 1.0f);
-			m_Scene->getWorld()->directLight.direction = glm::vec3(cos(angle) * cos(hAngle), sin(angle) * cos(hAngle), sin(hAngle));
-		}, glm::radians(180.0f), 0.0f, 60.0f);
-	}
+    auto lightFun = [this](f32 angle) {
+      constexpr float hAngle = glm::radians(10.0f);
 
-	void setupInputActions()
-	{
-		/**********************************************
-		 *                   UI                       *
-		 **********************************************/
-		InputAction pauseAction{};
-		pauseAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_ESCAPE}} };
-		pauseAction.gamepadButtonBindings = {
-				GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_START}} };
-		Input::setAction("TOGGLE_PAUSE", pauseAction);
+      m_Scene->getWorld()->directLight.color =
+          glm::vec4(1.0f, 0.2f + 0.7f * std::pow(sin(angle), 3.0f),
+                    std::pow(sin(angle), 5.0f) * 0.9f, 1.0f);
+      m_Scene->getWorld()->directLight.direction = glm::vec3(
+          cos(angle) * cos(hAngle), sin(angle) * cos(hAngle), sin(hAngle));
+    };
 
-		InputAction restartAction{};
-		restartAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_R}} };
-		restartAction.gamepadButtonBindings = {
-				GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_START}} };
-		Input::setAction("RESTART", restartAction);
+    auto lightInvFun = [this](f32 angle) {
+      constexpr float hAngle = glm::radians(10.0f);
+      f32 decay = 1.0f - std::pow(sin(angle), 5.0f);
+      m_Scene->getWorld()->directLight.color =
+          static_cast<f32>(1.0f - 0.5f * sin(angle)) *
+          glm::vec4(decay * 1.0f, decay * 0.2f, 1.0f - decay, 1.0f);
+      m_Scene->getWorld()->directLight.direction = glm::vec3(
+          cos(angle) * cos(hAngle), sin(angle) * cos(hAngle), sin(hAngle));
+    };
 
-		/**********************************************
-		 *                MOVEMENT                    *
-		 **********************************************/
-		InputAction leftAction{};
-		leftAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_A}} };
-		leftAction.gamepadAxisBindings = {
-				GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_X, AXIS_NEG}}} };
-		Input::setAction("MOVE_LEFT", leftAction);
+    auto *directLightTween = m_Scene->makeTween()->loop();
+    directLightTween->addCallbackTweener(
+        [this]() { m_Scene->setSkybox(SKYBOX_MORNING); });
+    directLightTween->addMethodTweener<f32>(lightFun, 0.0f, glm::radians(30.0f),
+                                            15.0f);
+    directLightTween->addCallbackTweener(
+        [this]() { m_Scene->setSkybox(SKYBOX_DAY); });
+    directLightTween->addMethodTweener<f32>(lightFun, glm::radians(30.0f),
+                                            glm::radians(150.0f), 60.0f);
+    directLightTween->addCallbackTweener(
+        [this]() { m_Scene->setSkybox(SKYBOX_SUNSET); });
+    directLightTween->addMethodTweener<f32>(lightFun, glm::radians(150.0f),
+                                            glm::radians(180.0f), 10.0f);
+    directLightTween->addCallbackTweener(
+        [this]() { m_Scene->setSkybox(SKYBOX_MIDNIGHT); });
+    directLightTween->addMethodTweener<f32>(lightInvFun, glm::radians(180.0f),
+                                            glm::radians(30.0f), 10.0f);
+    directLightTween->addCallbackTweener(
+        [this]() { m_Scene->setSkybox(SKYBOX_NIGHT); });
+    directLightTween->addMethodTweener<f32>(lightInvFun, glm::radians(150.0f),
+                                            0.0f, 50.0f);
+  }
 
-		InputAction rightAction{};
-		rightAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_D}} };
-		rightAction.gamepadAxisBindings = {
-				GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_X, AXIS_POS}}} };
-		Input::setAction("MOVE_RIGHT", rightAction);
+  void setupInputActions() {
+    /**********************************************
+     *                   UI                       *
+     **********************************************/
+    InputAction pauseAction{};
+    pauseAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_ESCAPE}}};
+    pauseAction.gamepadButtonBindings = {
+        GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_START}}};
+    Input::setAction("TOGGLE_PAUSE", pauseAction);
 
-		InputAction upAction{};
-		upAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_W}} };
-		upAction.gamepadAxisBindings = {
-				GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_Y, AXIS_NEG}}} };
-		Input::setAction("MOVE_UP", upAction);
+    InputAction restartAction{};
+    restartAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_R}}};
+    restartAction.gamepadButtonBindings = {
+        GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_START}}};
+    Input::setAction("RESTART", restartAction);
 
-		InputAction downAction{};
-		downAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_S}} };
-		downAction.gamepadAxisBindings = {
-				GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_Y, AXIS_POS}}} };
-		Input::setAction("MOVE_DOWN", downAction);
+    /**********************************************
+     *                MOVEMENT                    *
+     **********************************************/
+    InputAction leftAction{};
+    leftAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_A}}};
+    leftAction.gamepadAxisBindings = {
+        GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_X, AXIS_NEG}}}};
+    Input::setAction("MOVE_LEFT", leftAction);
 
-		/**********************************************
-		 *                  ACTIONS                   *
-		 **********************************************/
-		InputAction fireAction{};
-		fireAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_SPACE}} };
-		fireAction.mouseBindings = {
-				MouseBinding{{GLFW_MOUSE_BUTTON_1}} };
-		fireAction.gamepadButtonBindings = {
-				GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER}}
-		};
-		Input::setAction("FIRE", fireAction);
+    InputAction rightAction{};
+    rightAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_D}}};
+    rightAction.gamepadAxisBindings = {
+        GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_X, AXIS_POS}}}};
+    Input::setAction("MOVE_RIGHT", rightAction);
 
-		InputAction dashAction{};
-		dashAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_LEFT_SHIFT}}
-		};
-		dashAction.gamepadButtonBindings = {
-				GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_LEFT_BUMPER }}
-		};
-		Input::setAction("DASH", dashAction);
+    InputAction upAction{};
+    upAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_W}}};
+    upAction.gamepadAxisBindings = {
+        GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_Y, AXIS_NEG}}}};
+    Input::setAction("MOVE_UP", upAction);
 
-		/**********************************************
-		 *                  CAMERA                    *
-		 **********************************************/
+    InputAction downAction{};
+    downAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_S}}};
+    downAction.gamepadAxisBindings = {
+        GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_LEFT_Y, AXIS_POS}}}};
+    Input::setAction("MOVE_DOWN", downAction);
 
-		InputAction lookLeftAction{};
-		lookLeftAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_LEFT}},
-		};
-		lookLeftAction.gamepadAxisBindings = {
-				GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_X, AXIS_NEG}}} };
-		lookLeftAction.mouseAxisBindings = {
-				MouseAxisBinding{{Axis::NEGATIVE_X}} };
-		Input::setAction("LOOK_LEFT", lookLeftAction);
+    /**********************************************
+     *                  ACTIONS                   *
+     **********************************************/
+    InputAction fireAction{};
+    fireAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_SPACE}}};
+    fireAction.mouseBindings = {MouseBinding{{GLFW_MOUSE_BUTTON_1}}};
+    fireAction.gamepadButtonBindings = {
+        GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER}}};
+    Input::setAction("FIRE", fireAction);
 
-		InputAction lookRightAction{};
-		lookRightAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_RIGHT}},
-		};
-		lookRightAction.gamepadAxisBindings = {
-				GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_X, AXIS_POS}}} };
-		lookRightAction.mouseAxisBindings = {
-				MouseAxisBinding{{Axis::POSITIVE_X}} };
-		Input::setAction("LOOK_RIGHT", lookRightAction);
+    InputAction dashAction{};
+    dashAction.keyboardBindings = {KeyboardBinding{{GLFW_KEY_LEFT_SHIFT}}};
+    dashAction.gamepadButtonBindings = {
+        GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_LEFT_BUMPER}}};
+    Input::setAction("DASH", dashAction);
 
-		InputAction lookUpAction{};
-		lookUpAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_UP}},
-		};
-		lookUpAction.gamepadAxisBindings = {
-				GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_Y, AXIS_NEG}}} };
-		lookUpAction.mouseAxisBindings = {
-				MouseAxisBinding{{Axis::NEGATIVE_Y}} };
-		Input::setAction("LOOK_UP", lookUpAction);
+    /**********************************************
+     *                  CAMERA                    *
+     **********************************************/
 
-		InputAction lookDownAction{};
-		lookDownAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_DOWN}},
-		};
-		lookDownAction.gamepadAxisBindings = {
-				GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_Y, AXIS_POS}}} };
-		lookDownAction.mouseAxisBindings = {
-				MouseAxisBinding{{Axis::POSITIVE_Y}} };
-		Input::setAction("LOOK_DOWN", lookDownAction);
+    InputAction lookLeftAction{};
+    lookLeftAction.keyboardBindings = {
+        KeyboardBinding{{GLFW_KEY_LEFT}},
+    };
+    lookLeftAction.gamepadAxisBindings = {
+        GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_X, AXIS_NEG}}}};
+    lookLeftAction.mouseAxisBindings = {MouseAxisBinding{{Axis::NEGATIVE_X}}};
+    Input::setAction("LOOK_LEFT", lookLeftAction);
 
-		/**********************************************
-		 *                  DEBUG                     *
-		 **********************************************/
+    InputAction lookRightAction{};
+    lookRightAction.keyboardBindings = {
+        KeyboardBinding{{GLFW_KEY_RIGHT}},
+    };
+    lookRightAction.gamepadAxisBindings = {
+        GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_X, AXIS_POS}}}};
+    lookRightAction.mouseAxisBindings = {MouseAxisBinding{{Axis::POSITIVE_X}}};
+    Input::setAction("LOOK_RIGHT", lookRightAction);
 
-		InputAction toggleInfoAction{};
-		toggleInfoAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_F3}},
-		};
-		toggleInfoAction.gamepadButtonBindings = {
-				GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_BACK}}
-		};
-		Input::setAction("TOGGLE_INFO", toggleInfoAction);
+    InputAction lookUpAction{};
+    lookUpAction.keyboardBindings = {
+        KeyboardBinding{{GLFW_KEY_UP}},
+    };
+    lookUpAction.gamepadAxisBindings = {
+        GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_Y, AXIS_NEG}}}};
+    lookUpAction.mouseAxisBindings = {MouseAxisBinding{{Axis::NEGATIVE_Y}}};
+    Input::setAction("LOOK_UP", lookUpAction);
 
-		InputAction toggleGodmodeAction{};
-		toggleGodmodeAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_F1}},
-		};
-		Input::setAction("TOGGLE_GODMODE", toggleGodmodeAction);
+    InputAction lookDownAction{};
+    lookDownAction.keyboardBindings = {
+        KeyboardBinding{{GLFW_KEY_DOWN}},
+    };
+    lookDownAction.gamepadAxisBindings = {
+        GamepadAxisBinding{{{GLFW_GAMEPAD_AXIS_RIGHT_Y, AXIS_POS}}}};
+    lookDownAction.mouseAxisBindings = {MouseAxisBinding{{Axis::POSITIVE_Y}}};
+    Input::setAction("LOOK_DOWN", lookDownAction);
 
-		InputAction toggleSkyboxAction{};
-		toggleSkyboxAction.keyboardBindings = {
-				KeyboardBinding{{GLFW_KEY_M}},
-		};
-		toggleSkyboxAction.gamepadButtonBindings = {
-				GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_Y}}
-		};
-		Input::setAction("TOGGLE_SKYBOX", toggleSkyboxAction);
-	}
+    /**********************************************
+     *                  DEBUG                     *
+     **********************************************/
+
+    InputAction toggleInfoAction{};
+    toggleInfoAction.keyboardBindings = {
+        KeyboardBinding{{GLFW_KEY_F3}},
+    };
+    toggleInfoAction.gamepadButtonBindings = {
+        GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_BACK}}};
+    Input::setAction("TOGGLE_INFO", toggleInfoAction);
+
+    InputAction toggleGodmodeAction{};
+    toggleGodmodeAction.keyboardBindings = {
+        KeyboardBinding{{GLFW_KEY_F1}},
+    };
+    Input::setAction("TOGGLE_GODMODE", toggleGodmodeAction);
+
+    InputAction toggleSkyboxAction{};
+    toggleSkyboxAction.keyboardBindings = {
+        KeyboardBinding{{GLFW_KEY_M}},
+    };
+    toggleSkyboxAction.gamepadButtonBindings = {
+        GamepadButtonBinding{{GLFW_GAMEPAD_BUTTON_Y}}};
+    Input::setAction("TOGGLE_SKYBOX", toggleSkyboxAction);
+  }
 };
 
 } // namespace game
 
-int main()
-{
-	using namespace vulture;
+int main() {
+  using namespace vulture;
 
-	Logger logger("output.log");
-	try
-	{
-		Ref<Application> app; // The game must be destroyed before the application
-		{
-			game::TestGame game;
+  Logger logger("output.log");
+  try {
+    Ref<Application> app; // The game must be destroyed before the application
+    {
+      game::TestGame game;
 
-			app = Application::launch(game, vulture::AppConfig{ "Vulture demo", 800, 600 });
-		}
+      app = Application::launch(game,
+                                vulture::AppConfig{"Vulture demo", 800, 600});
+    }
 
-		return EXIT_SUCCESS;
-	}
-	catch (const std::exception& exception)
-	{
-		VUFATAL("An exception was thrown.\nMessage: %s\n\nShutting down!", exception.what());
-	}
+    return EXIT_SUCCESS;
+  } catch (const std::exception &exception) {
+    VUFATAL("An exception was thrown.\nMessage: %s\n\nShutting down!",
+            exception.what());
+  }
 
-	return EXIT_FAILURE;
+  return EXIT_FAILURE;
 }
